78590cfbe521f960b1e0ea16760ba8f25775088a
Steven Shaw <steshaw@gmail.com>
2010-01-26 21:55:51 +1000

    more go

---
+++ go/printers.go
@@ -0,0 +1,16 @@
+package main
+
+import "time"
+
+func server(i int) {
+    for {
+        println(i);
+        time.Sleep(1)
+    }
+}
+
+func main() {
+    go server(1);
+    go server(2);
+    time.Sleep(2000);
+}
