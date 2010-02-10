b490aceecbcb71d445d67adf98c121ae7c1eefab
Steven Shaw <steshaw@gmail.com>
2010-02-10 22:07:41 +1000

    Generify Tree

--- tree-number/TreeNumberInJava.java
+++ tree-number/TreeNumberInJava.java
@@ -13,45 +13,57 @@ public class TreeNumberInJava {
   interface F<A, B> { public B f(A a); }
 
   /**
-   * Tree algebraic data type using Church encoding
+   * Geneified Tree algebraic data type using Church encoding
    * @see http://apocalisp.wordpress.com/2009/08/21/structural-pattern-matching-in-java/
    */
-  public static abstract class Tree {
+  public static abstract class Tree<A> {
 
     // Constructor private so the type is sealed.
     private Tree() {}
 
-    public abstract <T> T match(F<Empty, T> a, F<Leaf, T> b, F<Branch, T> c);
+    public abstract <T> T match(F<Empty<A>, T> a, F<Leaf<A>, T> b, F<Branch<A>, T> c);
 
-    public static final class Empty extends Tree {
-      public <T> T match(F<Empty, T> a, F<Leaf, T> b, F<Branch, T> c) {
+    public static final class Empty<A> extends Tree<A> {
+      public <T> T match(F<Empty<A>, T> a, F<Leaf<A>, T> b, F<Branch<A>, T> c) {
         return a.f(this);
       }
 
       public Empty() {}
     }
 
-    public static final class Leaf extends Tree {
-      public final int n;
+    public static final class Leaf<A> extends Tree<A> {
+      public final A a;
 
-      public <T> T match(F<Empty, T> a, F<Leaf, T> b, F<Branch, T> c) {
+      public <T> T match(F<Empty<A>, T> a, F<Leaf<A>, T> b, F<Branch<A>, T> c) {
         return b.f(this);
       }
 
-      public Leaf(int n) { this.n = n; }
+      public Leaf(A a) { this.a = a; }
     }
 
-    public static final class Branch extends Tree {
-      public final Tree left;
-      public final Tree right;    
+    public static final class Branch<A> extends Tree<A> {
+      public final Tree<A> left;
+      public final Tree<A> right;
 
-      public <T> T match(F<Empty, T> a, F<Leaf, T> b, F<Branch, T> c) {
+      public <T> T match(F<Empty<A>, T> a, F<Leaf<A>, T> b, F<Branch<A>, T> c) {
         return c.f(this);
       }
 
-      public Branch(Tree left, Tree right) {
+      public Branch(Tree<A> left, Tree<A> right) {
         this.left = left; this.right = right;
       }
+    }
+  }
+
+  static class Trees {
+    public static <A> Tree.Empty<A> empty() {
+      return new Tree.Empty<A>();
+    }
+    public static <A> Tree.Leaf<A> node(A a) {
+      return new Tree.Leaf<A>(a);
+    }
+    public static <A> Tree.Branch<A> branch(Tree<A> left, Tree<A> right) {
+      return new Tree.Branch<A>(left, right);
     }
   }
 
@@ -103,27 +115,40 @@ public class TreeNumberInJava {
     }
   }
 
-  public static <Ignore> F<Ignore, Integer> constant(final Integer i) {
-    return new F<Ignore, Integer>() {
-      public Integer f(Ignore ignore) { return i; };
+  public static <Ignore, B> F<Ignore, B> constant(final B b) {
+    return new F<Ignore, B>() {
+      public B f(Ignore ignore) { return b; };
     };
   }
 
-  private static F<Tree.Empty, Integer> zeroOnEmpty = new F<Tree.Empty, Integer>() {
-    public Integer f(Tree.Empty tree) {
-      return 0;
-    }
-  };
-  private static F<Tree.Leaf, Integer> oneOnLeaf = new F<Tree.Leaf, Integer>() {
-    public Integer f(Tree.Leaf tree) {
-      return 1;
-    }
-  };
+  private static <A> F<Tree.Empty<A>, Integer> zeroOnEmpty() {
+      return new F<Tree.Empty<A>, Integer>() {
+        public Integer f(Tree.Empty<A> tree) {
+          return 0;
+        }
+      };
+  }
 
-  public static int depth(Tree t) {
-    //return t.match(constant<Tree.Empty>(0), constant<Tree.Leaf>(1), new F<Tree.Branch, Integer>(){
-    return t.match(zeroOnEmpty, oneOnLeaf, new F<Tree.Branch, Integer>(){
-      public Integer f(Tree.Branch n) {
+  private static <A> F<Tree.Leaf<A>, Integer> oneOnLeaf() {
+      return new F<Tree.Leaf<A>, Integer>() {
+        public Integer f(Tree.Leaf tree) {
+          return 1;
+        }
+      };
+  }
+
+  public static <A> int depth(Tree<A> t) {
+    //return t.match(constant(0), constant(1), new F<Tree.Branch<A>, Integer>(){
+    return t.match(new F<Tree.Empty<A>, Integer>() {
+      public Integer f(Tree.Empty<A> t) {
+          return 0;
+      }
+    }, new F<Tree.Leaf<A>, Integer>() {
+      public Integer f(Tree.Leaf<A> t) {
+          return 1;
+      }
+    }, new F<Tree.Branch<A>, Integer>() {
+      public Integer f(Tree.Branch<A> n) {
         return 1 + max(depth(n.left), depth(n.right));
       }
     });
@@ -133,15 +158,18 @@ public class TreeNumberInJava {
     System.out.println(s);
   }
 
-  public static void main(String[] args) {
+/*
+  public static number(seed: Int): (Tree[(A, Int)], Int) = this match {
+    case Leaf(n) => (Leaf((n, seed)), seed+1)
+    case Branch(l, r) => {
+      val (lt, seed1) = l.number(seed)
+      val (rt, seed2) = r.number(seed1)
+      (Branch(lt, rt), seed2)
+  }
+*/
 
-    Tree t1 = new Tree.Branch(new Tree.Branch(new Tree.Leaf(2), new Tree.Leaf(6)), new Tree.Leaf(9));
-    Tree t2 = new Tree.Branch(new Tree.Branch(new Tree.Leaf(2), new Tree.Branch(new Tree.Leaf(4), new Tree.Leaf(5))), new Tree.Leaf(9));
-
-    println("depth of t1 " + depth(t1));
-    println("depth of t2 " + depth(t2));
-
-    State<Double, String> state0 = new State<Double, String>() {
+  private static void stateTrial() {
+          State<Double, String> state0 = new State<Double, String>() {
       public Pair<Double, String> run(Double s) {
         return Pairs.pair(s, "9");
       }
@@ -165,6 +193,30 @@ public class TreeNumberInJava {
       }
     });
     System.out.println("state2 = " + state2.run(1.0));
+  }
+
+  public static void main(String[] args) {    
+    Tree<Integer> empty = new Tree.Empty<Integer>();
+    Tree<Integer> leaf1 = new Tree.Leaf<Integer>(1);
+    Tree<Integer> leaf2 = new Tree.Leaf<Integer>(2);
+    Tree<Integer> leaf3 = new Tree.Leaf<Integer>(3);
+    Tree<Integer> leaf4 = new Tree.Leaf<Integer>(4);
+    Tree<Integer> leaf5 = new Tree.Leaf<Integer>(5);
+    Tree<Integer> leaf6 = new Tree.Leaf<Integer>(6);
+    Tree<Integer> leaf7 = new Tree.Leaf<Integer>(7);
+    Tree<Integer> leaf8 = new Tree.Leaf<Integer>(8);
+    Tree<Integer> leaf9 = new Tree.Leaf<Integer>(9);
+
+    Tree<Integer> t1 = new Tree.Branch<Integer>(new Tree.Branch<Integer>(leaf2, leaf6), leaf9);
+    Tree<Integer> t2 = new Tree.Branch<Integer>(new Tree.Branch<Integer>(leaf2, new Tree.Branch<Integer>(leaf4, leaf5)), leaf9);
+
+    println("depth of empty tree = " + depth(empty));
+    println("depth of leaf tree = " + depth(leaf1));
+    println("depth of empty branch tree = " + depth(new Tree.Branch<Integer>(empty, empty)));
+    println("depth of t1 " + depth(t1));
+    println("depth of t2 " + depth(t2));
+
+    stateTrial();
   }
 
 }
