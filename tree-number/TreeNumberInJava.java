44865b7fbc63d0b0d6d70d3c7a4c31c26bf34e25
Steven Shaw <steshaw@gmail.com>
2010-02-10 23:23:22 +1000

    Got non-state-monad tree numbering in Java working (but ugly)

--- tree-number/TreeNumberInJava.java
+++ tree-number/TreeNumberInJava.java
@@ -59,7 +59,7 @@ public class TreeNumberInJava {
     public static <A> Tree.Empty<A> empty() {
       return new Tree.Empty<A>();
     }
-    public static <A> Tree.Leaf<A> node(A a) {
+    public static <A> Tree.Leaf<A> leaf(A a) {
       return new Tree.Leaf<A>(a);
     }
     public static <A> Tree.Branch<A> branch(Tree<A> left, Tree<A> right) {
@@ -78,7 +78,8 @@ public class TreeNumberInJava {
         public A first() { return a; }
         public B second() { return b; }
         public String toString() {
-          return "(" + a + ":" + a.getClass().getName() + ", " + b + ":" + b.getClass().getName() + ")";
+          // Output like in Scala.
+          return "(" + a + "," + b + ")";
         }
       };
     }
@@ -115,20 +116,23 @@ public class TreeNumberInJava {
     }
   }
 
-  public static <Ignore, B> F<Ignore, B> constant(final B b) {
-    return new F<Ignore, B>() {
-      public B f(Ignore ignore) { return b; };
+  // XXX: This does not work. Type-check fails in use.
+  static <A, B> F<Tree<A>, B> constant(final B b) {
+    return new F<Tree<A>, B>() {
+      public B f(Tree<A> ignore) { return b; };
     };
   }
 
+  // XXX: This cannot be used either due to type-check failure.
   private static <A> F<Tree.Empty<A>, Integer> zeroOnEmpty() {
-      return new F<Tree.Empty<A>, Integer>() {
-        public Integer f(Tree.Empty<A> tree) {
-          return 0;
-        }
-      };
+    return new F<Tree.Empty<A>, Integer>() {
+      public Integer f(Tree.Empty<A> tree) {
+        return 0;
+      }
+    };
   }
 
+  // XXX: ditto here :(
   private static <A> F<Tree.Leaf<A>, Integer> oneOnLeaf() {
       return new F<Tree.Leaf<A>, Integer>() {
         public Integer f(Tree.Leaf tree) {
@@ -138,7 +142,12 @@ public class TreeNumberInJava {
   }
 
   public static <A> int depth(Tree<A> t) {
-    //return t.match(constant(0), constant(1), new F<Tree.Branch<A>, Integer>(){
+    // XXX: Type-check fails in use of constant(n)
+//    return t.match(constant(0), constant(1), new F<Tree.Branch<A>, Integer>(){
+//      public Integer f(Tree.Branch<A> n) {
+//        return 1 + max(depth(n.left), depth(n.right));
+//      }
+//    });
     return t.match(new F<Tree.Empty<A>, Integer>() {
       public Integer f(Tree.Empty<A> t) {
           return 0;
@@ -159,6 +168,7 @@ public class TreeNumberInJava {
   }
 
 /*
+  // Scala version of tree-numbering for reference.
   public static number(seed: Int): (Tree[(A, Int)], Int) = this match {
     case Leaf(n) => (Leaf((n, seed)), seed+1)
     case Branch(l, r) => {
@@ -167,6 +177,25 @@ public class TreeNumberInJava {
       (Branch(lt, rt), seed2)
   }
 */
+  // "Simple" non-state monad version of tree numbering.
+  public static <A> Pair<? extends Tree<Pair<A, Integer>>, Integer> number(Tree<A> t, final int seed) {
+    return t.match(new F<Tree.Empty<A>, Pair<? extends Tree<Pair<A, Integer>>, Integer>>() {
+      public Pair<? extends Tree<Pair<A, Integer>>, Integer> f(Tree.Empty<A> t) {
+          //return Pairs.pair(Trees.empty(), seed);
+        return Pairs.pair(new Tree.Empty<Pair<A, Integer>>(), seed);
+      }
+    }, new F<Tree.Leaf<A>, Pair<? extends Tree<Pair<A, Integer>>, Integer>>() {
+      public Pair<? extends Tree<Pair<A, Integer>>, Integer> f(Tree.Leaf<A> t) {
+          return Pairs.pair(Trees.leaf(Pairs.pair(t.a, seed)), seed+1);
+      }
+    }, new F<Tree.Branch<A>, Pair<? extends Tree<Pair<A, Integer>>, Integer>>() {
+      public Pair<? extends Tree<Pair<A, Integer>>, Integer> f(Tree.Branch<A> t) {
+        Pair<? extends Tree<Pair<A, Integer>>, Integer> lp = number(t.left, seed);
+        Pair<? extends Tree<Pair<A, Integer>>, Integer> rp = number(t.right, lp.second());
+        return Pairs.pair(Trees.branch(lp.first(), rp.first()), rp.second());
+      }
+    });
+  }
 
   private static void stateTrial() {
           State<Double, String> state0 = new State<Double, String>() {
@@ -195,28 +224,75 @@ public class TreeNumberInJava {
     System.out.println("state2 = " + state2.run(1.0));
   }
 
+  static void print(String s) {
+    System.out.print(s);
+  }
+
+  final static class Unit {
+    private Unit() {}
+    static Unit value = new Unit();
+  };
+
+  public static <A> void printTree(Tree<A> t) {
+    t.match(new F<Tree.Empty<A>, Unit>() {
+      public Unit f(Tree.Empty<A> t) {
+        print("Empty()");
+        return Unit.value;
+      }
+    }, new F<Tree.Leaf<A>, Unit>() {
+      public Unit f(Tree.Leaf<A> t) {
+        print("Leaf(" + t.a + ")");
+          return Unit.value;
+      }
+    }, new F<Tree.Branch<A>, Unit>() {
+      public Unit f(Tree.Branch<A> t) {
+        print("Branch(");
+        printTree(t.left);
+        print(",");
+        printTree(t.right);
+        print(")");
+        return Unit.value;
+      }
+    });
+  }
+
+  public static void go(Tree<Integer> t) {
+    printTree(t);
+    println("");
+    printTree(number(t, 10).first());
+    println("");
+  }
+
   public static void main(String[] args) {    
-    Tree<Integer> empty = new Tree.Empty<Integer>();
-    Tree<Integer> leaf1 = new Tree.Leaf<Integer>(1);
-    Tree<Integer> leaf2 = new Tree.Leaf<Integer>(2);
-    Tree<Integer> leaf3 = new Tree.Leaf<Integer>(3);
-    Tree<Integer> leaf4 = new Tree.Leaf<Integer>(4);
-    Tree<Integer> leaf5 = new Tree.Leaf<Integer>(5);
-    Tree<Integer> leaf6 = new Tree.Leaf<Integer>(6);
-    Tree<Integer> leaf7 = new Tree.Leaf<Integer>(7);
-    Tree<Integer> leaf8 = new Tree.Leaf<Integer>(8);
-    Tree<Integer> leaf9 = new Tree.Leaf<Integer>(9);
+    Tree<Integer> empty = Trees.empty();
+    Tree<Integer> leaf1 = Trees.leaf(1);
+    Tree<Integer> leaf2 = Trees.leaf(2);
+    Tree<Integer> leaf4 = Trees.leaf(4);
+    Tree<Integer> leaf5 = Trees.leaf(5);
+    Tree<Integer> leaf6 = Trees.leaf(6);
+    Tree<Integer> leaf9 = Trees.leaf(9);
 
-    Tree<Integer> t1 = new Tree.Branch<Integer>(new Tree.Branch<Integer>(leaf2, leaf6), leaf9);
-    Tree<Integer> t2 = new Tree.Branch<Integer>(new Tree.Branch<Integer>(leaf2, new Tree.Branch<Integer>(leaf4, leaf5)), leaf9);
+    Tree<Integer> t1 = Trees.branch(Trees.branch(leaf2, leaf6), leaf9);
+    Tree<Integer> t2 = Trees.branch(Trees.branch(leaf2, Trees.branch(leaf4, leaf5)), leaf9);
 
-    println("depth of empty tree = " + depth(empty));
-    println("depth of leaf tree = " + depth(leaf1));
-    println("depth of empty branch tree = " + depth(new Tree.Branch<Integer>(empty, empty)));
-    println("depth of t1 " + depth(t1));
-    println("depth of t2 " + depth(t2));
+    // Since we only have one algorithm, just run the same one twice with a line in between to match the "expected" file.
+    go(t1);
+    go(t2);
+    println("");
 
-    stateTrial();
+    go(t1);
+    go(t2);
+
+    boolean justDoTreeNumbering = false;
+    if (justDoTreeNumbering) {
+      println("depth of empty tree = " + depth(empty));
+      println("depth of leaf tree = " + depth(leaf1));
+      println("depth of empty branch tree = " + depth(new Tree.Branch<Integer>(empty, empty)));
+      println("depth of t1 " + depth(t1));
+      println("depth of t2 " + depth(t2));
+
+      stateTrial();
+    }
   }
 
 }
--- tree-number/test.sh
+++ tree-number/test.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+javac -d . *.java &&
 scalac *.scala &&
   scala steshaw.TreeNumber | diff expected - && echo TreeNumber ok &&
   scala steshaw.TreeNumberWithScalaz5StateMonad | diff expected - && echo TreeNumberWithScalaz5StateMonad ok &&
   scala steshaw.TreeNumberWithStateMonad | diff expected - && echo TreeNumberWithStateMonad ok
+  java steshaw.TreeNumberInJava | diff expected - && echo TreeNumberInJava ok
