ba7a0f8293713f1a2dcaf6bf6ca5482e8511cc3f
Steven Shaw <steshaw@gmail.com>
2010-10-25 22:09:35 +1000

    Update api to llvm-2.8

--- ocaml/llvm/hello.ml
+++ ocaml/llvm/hello.ml
@@ -2,30 +2,31 @@ open Printf
 open Llvm
 
 let main filename =
-   let m = create_module filename in
+   let ctx = global_context () in
+   let m = create_module ctx filename in
 
    (* @greeting = global [14 x i8] c"Hello, world!\00" *)
    let greeting =
-     define_global "greeting" (const_string "Hello, world!\000") m in
+     define_global "greeting" (const_string ctx "Hello, world!\000") m in
 
    (* declare i32 @puts(i8* ) *)
    let puts =
      declare_function "puts"
-       (function_type i32_type [|pointer_type i8_type|]) m in
+       (function_type (i32_type ctx) [|pointer_type (i8_type ctx)|]) m in
 
    (* define i32 @main() { entry: *)
-   let main = define_function "main" (function_type i32_type [| |]) m in
-   let at_entry = builder_at_end (entry_block main) in
+   let main = define_function "main" (function_type (i32_type ctx) [| |]) m in
+   let at_entry = builder_at_end ctx (entry_block main) in
 
    (* %tmp = getelementptr [14 x i8]* @greeting, i32 0, i32 0 *)
-   let zero = const_int i32_type 0 in
+   let zero = const_int (i32_type ctx) 0 in
    let str = build_gep greeting [| zero; zero |] "tmp" at_entry in
 
    (* call i32 @puts( i8* %tmp ) *)
    ignore (build_call puts [| str |] "" at_entry);
 
    (* ret void *)
-   ignore (build_ret (const_null i32_type) at_entry);
+   ignore (build_ret (const_null (i32_type ctx)) at_entry);
 
    (* write the module to a file *)
    if not (Llvm_bitwriter.write_bitcode_file m filename) then exit 1;
@@ -34,18 +35,3 @@ let main filename =
 let () = match Sys.argv with
   | [|_; filename|] -> main filename
   | _ -> main "a.out"
-
-(*
-To use it I just do:
-
-$ ocamlopt -dtypes -cc g++ -I /usr/local/lib/ocaml/ llvm.cmxa 
-llvm_bitwriter.cmxa hellow.ml -o hellow
-$ ./hellow run.bc
-$ llc -f -march=c run.bc -o run.c
-$ gcc run.c -o run
-run.c:114: warning: conflicting types for built-in function ‘malloc’
-run.c: In function ‘main’:
-run.c:143: warning: return type of ‘main’ is not ‘int’
-$ ./run
-Hello, world!
-*)
--- ocaml/llvm/metahelloworld.ml
+++ ocaml/llvm/metahelloworld.ml
@@ -12,31 +12,36 @@ open Llvm_bitwriter
 
 let _ =
    let filename = Sys.argv.(1) in
-   let m = create_module filename in
+   let ctx = global_context () in
+   let m = create_module ctx filename in
 
    (* @greeting = global [14 x i8] c"Hello, world!\00" *)
+(*
    let greeting = define_global "greeting" (make_string_constant
                                               "Hello, world!" true) m in
+*)
+   let greeting =
+     define_global "greeting" (const_stringz ctx "Hello, world!\000") m in
 
    (* declare i32 @puts(i8) *)
-   let puts = declare_function "puts" (make_function_type i32_type [|
-                                         make_pointer_type i8_type |]  false) m in
+   let puts = declare_function "puts" (function_type (i32_type ctx) [|
+                                         pointer_type (i8_type ctx) |]) m in
 
    (* define i32 @main() {
       entry:               *)
-   let main = define_function "main" (make_function_type
-                                        i32_type [| |] false) m in
-   let at_entry = builder_at_end (entry_block main) in
+   let main = define_function "main" (function_type
+                                        (i32_type ctx) [| |] ) m in
+   let at_entry = builder_at_end ctx (entry_block main) in
 
    (* %tmp = getelementptr [14 x i8]* @greeting, i32 0, i32 0 *)
-   let zero = make_int_constant i32_type 0 false in
+   let zero = const_int (i32_type ctx) 0 in
    let str = build_gep greeting [| zero; zero |] "tmp" at_entry in
 
    (* call i32 @puts( i8* %tmp ) *)
    ignore (build_call puts [| str |] "" at_entry);
 
    (* ret void *)
-   ignore (build_ret (make_null i32_type) at_entry);
+   ignore (build_ret (const_null (i32_type ctx)) at_entry);
 
    (* write the module to a file *)
    if not (write_bitcode_file m filename) then exit 1;
