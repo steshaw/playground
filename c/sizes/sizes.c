9636a9d49a458dff9ad8ea4ec38f82cd21754f28
Steven Shaw <steshaw@gmail.com>
2010-10-25 21:13:11 +1000

    added c/sizes/

---
+++ c/sizes/sizes.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+  printf("sizeof(char) = %ld\n", sizeof(char));
+  printf("sizeof(short) = %ld\n", sizeof(short));
+  printf("sizeof(int) = %ld\n", sizeof(int));
+  printf("sizeof(long) = %ld\n", sizeof(long));
+  return 0;
+}
