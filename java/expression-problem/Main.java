7e40c886b3a6dbc94d14ddc89717eff1f838f67b
Steven Shaw <steven@steshaw.org>
2011-03-30 19:15:04 +1000

    Got expression problem solution compiling but only with an evil cast :(

--- java/expression-problem/Main.java
+++ java/expression-problem/Main.java
@@ -1,14 +1,14 @@
-class LangF<This extends LangF<This>> {
+class LangF<T extends LangF<T>> {
   interface Visitor<R> {
     public R forNum(int n);
   }
   interface Exp {
-    public <R> R visit(This.Visitor<R> v);
+    public <R> R visit(/*T*/LangF.Visitor<R> v);
   }
   class Num implements Exp {
     protected final int n_;
     public Num(int n) {n_=n;}
-    public <R> R visit(This.Visitor<R> v) {
+    public <R> R visit(/*T.*/Visitor<R> v) {
       return v.forNum(n_);
     }
   }
@@ -20,19 +20,21 @@ class LangF<This extends LangF<This>> {
 }
 final class Lang extends LangF<Lang> {}
 
-class Lang2F<This extends Lang2F<This>> extends LangF<This> {
-  interface Visitor<R> extends LangF<This>.Visitor<R> {
-    public R forPlus(This.Exp e1, This.Exp e2);
+class Lang2F<T extends Lang2F<T>> extends LangF<T> {
+  interface Visitor<R> extends LangF/*<T>*/.Visitor<R> {
+    public R forPlus(/*T.*/Exp e1, /*T.*/Exp e2);
   }
   class Plus implements Exp {
-    protected final This.Exp e1_,e2_;
-    public Plus(This.Exp e1, This.Exp e2) {e1_=e1; e2_=e2;}
-    public <R> R visit(This.Visitor<R> v) {
-      return v.forPlus(e1_,e2_);
+    protected final /*T.*/Exp e1_,e2_;
+    public Plus(/*T.*/Exp e1, /*T.*/Exp e2) {e1_=e1; e2_=e2;}
+
+    public <R> R visit(/*T.*/LangF.Visitor<R> v) {
+      Visitor<R> v2 = (Visitor)v; // FIXME: cast.
+      return v2.forPlus(e1_,e2_);
     }
   }
-  class Eval extends LangF<This>.Eval implements Visitor<Integer> {
-    public Integer forPlus(This.Exp e1, This.Exp e2) {
+  class Eval extends LangF<T>.Eval implements Visitor<Integer> {
+    public Integer forPlus(/*T.*/Exp e1, /*T.*/Exp e2) {
       return new Integer(
         e1.visit(this).intValue() + e2.visit(this).intValue()
       );
@@ -42,7 +44,7 @@ class Lang2F<This extends Lang2F<This>> extends LangF<This> {
     public String forNum(int n) {
       return Integer.toString(n);
     }
-    public String forPlus(This.Exp e1, This.Exp e2) {
+    public String forPlus(/*T.*/Exp e1, /*T.*/Exp e2) {
       return "(" + e1.visit(this) + "+" + e2.visit(this) +")";
     }
   }
