82f51d7742cd55435b0e0621499b4a994a9db823
Steven Shaw <steven@steshaw.org>
2011-03-31 18:17:56 +1000

    Some improvement using a odd looking factory

--- java/expression-problem/data-centric/DataCentric.java
+++ java/expression-problem/data-centric/DataCentric.java
@@ -1,10 +1,24 @@
 public class DataCentric {
-  static <T extends Exp<T>> void fred() {
-    Exp<T> e = new Add<T>(new Lit(2),new Lit(3));
+  public static void original() {
+    ExpFixed lit1 = Factory.newLit(2);
+    ExpFixed lit2 = Factory.newLit(3);
+    Exp e = new Add<ExpFixed>(lit1, lit2);
     e.print();
     System.out.println();
   }
+  public static void next() {
+    ExpFixed lit1 = Factory.newLit(1);
+    ExpFixed lit2 = Factory.newLit(2);
+    ExpFixed lit3 = Factory.newLit(3);
+    ExpFixed e1 = Factory.newAdd(lit1, lit2);
+    ExpFixed e2 = Factory.newAdd(e1, lit3);
+    e1.print();
+    System.out.println();
+    e2.print();
+    System.out.println();
+  }
   public static void main(String[] args) {
-    fred();
+    original();
+    next();
   }
 }
--- java/expression-problem/data-centric/Exp.java
+++ java/expression-problem/data-centric/Exp.java
@@ -1,8 +1,9 @@
+
 interface Exp<T extends Exp<T>> {
   void print();
 }
 
-class Lit implements Exp {
+class Lit<T extends Exp<T>> implements Exp<T> {
   protected int value;
   Lit(int v) { value = v; }
   public void print() { System.out.print(value); }
@@ -12,4 +13,22 @@ class Add<T extends Exp<T>> implements Exp<T> {
   protected T left, right;
   Add(T l, T r) { left = l; right = r; }
   public void print() { left.print(); System.out.print('+'); right.print(); }
+}
+
+interface ExpFixed extends Exp<ExpFixed> {}
+
+class Factory {
+  public static ExpFixed newLit(int v) {
+    class LitFixed extends Lit<ExpFixed> implements ExpFixed {
+      LitFixed(int v) { super(v); }
+    }
+    return new LitFixed(v);
+  }
+
+  public static ExpFixed newAdd(ExpFixed left, ExpFixed right) {
+    class AddFixed extends Add<ExpFixed> implements ExpFixed {
+      AddFixed(ExpFixed e1, ExpFixed e2) { super(e1, e2); }
+    }
+    return new AddFixed(left, right);
+  }
 }
