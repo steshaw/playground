8316a78157776fb3040b5825efac6ff9781d9d5f
Steven Shaw <steven@steshaw.org>
2011-05-03 17:32:56 +1000

    Cleanup

--- opengl/nehe/lesson06/lesson06.cpp
+++ opengl/nehe/lesson06/lesson06.cpp
@@ -16,67 +16,48 @@
 #include <OpenGL/glu.h>
 #include <SDL/SDL.h>
 
-/* screen width, height, and bit depth */
 #define SCREEN_WIDTH  640
 #define SCREEN_HEIGHT 480
 #define SCREEN_BPP     16
 
-/* Set up some booleans */
-//#define TRUE  1
-//#define FALSE 0
-
-/* This is our SDL surface */
 static SDL_Surface *surface;
 
 static GLfloat xrot = 0.0f;
 static GLfloat yrot = 0.0f;
 static GLfloat zrot = 0.0f;
 
-int videoFlags  = SDL_OPENGL;          /* Enable OpenGL in SDL */
+int videoFlags  = SDL_OPENGL;
 
 static GLuint texture[1];
 
-/* function to release/destroy our resources and restoring the old desktop */
-void Quit(int returnCode) {
+void cleanExit(int returnCode) {
   SDL_Quit();
   exit(returnCode);
 }
 
-/* function to load in bitmap as a GL texture */
-int LoadGLTextures() {
-  /* Status indicator */
-  int Status = false;
+int loadTextures() {
+  SDL_Surface *textureImage[1];
+  textureImage[0] = SDL_LoadBMP("data/nehe.bmp");
+  if (!textureImage[0]) return false;
   
-  /* Create storage space for the texture */
-  SDL_Surface *TextureImage[1]; 
+  // Create the texture.
+  glGenTextures(1, &texture[0]);
   
-  /* Load The Bitmap, Check For Errors, If Bitmap's Not Found Quit */
-  if ((TextureImage[0] = SDL_LoadBMP("data/nehe.bmp"))) {
-    /* Set the status to true */
-    Status = true;
-    
-    /* Create The Texture */
-    glGenTextures( 1, &texture[0] );
-    
-    /* Typical Texture Generation Using Data From The Bitmap */
-    glBindTexture( GL_TEXTURE_2D, texture[0] );
-    
-    /* Generate The Texture */
-    glTexImage2D( GL_TEXTURE_2D, 0, 3, TextureImage[0]->w,
-                 TextureImage[0]->h, 0, GL_BGR,
-                 GL_UNSIGNED_BYTE, TextureImage[0]->pixels );
-    
-    /* Linear Filtering */
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR );
-    glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR );
-  }
+  // Typical texture generation using data from the bitmap.
+  glBindTexture(GL_TEXTURE_2D, texture[0]);
   
-  /* Free up any memory we may have used */
-  if (TextureImage[0]) {
-    SDL_FreeSurface(TextureImage[0]);
-  }
+  // Generate the texture.
+  glTexImage2D(GL_TEXTURE_2D, 0, 3, 
+               textureImage[0]->w, textureImage[0]->h, 
+               0, GL_BGR,
+               GL_UNSIGNED_BYTE, textureImage[0]->pixels);
   
-  return Status;
+  // Linear Filtering.
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  SDL_FreeSurface(textureImage[0]);
+  return true;
 }
 
 int resizeWindow(int width, int height) {
@@ -109,7 +90,7 @@ void handleKeyPress(SDL_keysym *keysym) {
 	case SDLK_ESCAPE:
     case SDLK_q:
       printf("q/Esc pressed\n");
-      Quit(0);
+      cleanExit(0);
       break;
     case SDLK_f:
       printf("f pressed\n");
@@ -132,39 +113,29 @@ void handleKeyPress(SDL_keysym *keysym) {
 
 /* general OpenGL initialization function */
 int initGL() {
-  /* Load in the texture */
-  if ( !LoadGLTextures( ) )
-	return false;
+  if (!loadTextures()) return false;
   
-  /* Enable Texture Mapping ( NEW ) */
-  glEnable( GL_TEXTURE_2D );
-  
-  /* Enable smooth shading */
-  glShadeModel( GL_SMOOTH );
+  glEnable(GL_TEXTURE_2D);
+  glShadeModel(GL_SMOOTH);
   
   /* Set the background black */
   glClearColor( 0.0f, 0.0f, 0.0f, 0.5f );
   
-  /* Depth buffer setup */
-  glClearDepth( 1.0f );
+  glClearDepth(1.0f);
   
-  /* Enables Depth Testing */
-  glEnable( GL_DEPTH_TEST );
+  glEnable(GL_DEPTH_TEST);  
+  glDepthFunc(GL_LEQUAL);
   
-  /* The Type Of Depth Test To Do */
-  glDepthFunc( GL_LEQUAL );
-  
-  /* Really Nice Perspective Calculations */
-  glHint( GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST );
+  glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
   
   return true;
 }
 
 /* Here goes our drawing code */
-int drawGLScene() {
+void drawScene() {
   /* These are to calculate our fps */
-  static GLint T0     = 0;
-  static GLint Frames = 0;
+  static GLint t0         = 0;
+  static GLint frameCount = 0;
   
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   
@@ -176,8 +147,7 @@ int drawGLScene() {
   glRotatef(yrot, 0.0f, 1.0f, 0.0f);
   glRotatef(zrot, 0.0f, 0.0f, 1.0f);
   
-  /* Select Our Texture */
-  glBindTexture( GL_TEXTURE_2D, texture[0] );
+  glBindTexture(GL_TEXTURE_2D, texture[0]);
   
   /* NOTE:
    *   The x coordinates of the glTexCoord2f function need to inverted
@@ -186,88 +156,87 @@ int drawGLScene() {
    * now read glTexCoord2f( 0.0f, 0.0f );
    */
   glBegin(GL_QUADS);
-  /* Front Face */
-  /* Bottom Left Of The Texture and Quad */
-  glTexCoord2f( 0.0f, 1.0f ); glVertex3f( -1.0f, -1.0f, 1.0f );
-  /* Bottom Right Of The Texture and Quad */
-  glTexCoord2f( 1.0f, 1.0f ); glVertex3f(  1.0f, -1.0f, 1.0f );
-  /* Top Right Of The Texture and Quad */
-  glTexCoord2f( 1.0f, 0.0f ); glVertex3f(  1.0f,  1.0f, 1.0f );
-  /* Top Left Of The Texture and Quad */
-  glTexCoord2f( 0.0f, 0.0f ); glVertex3f( -1.0f,  1.0f, 1.0f );
+  {    
+    /* Front Face */
+    /* Bottom Left Of The Texture and Quad */
+    glTexCoord2f( 0.0f, 1.0f ); glVertex3f( -1.0f, -1.0f, 1.0f );
+    /* Bottom Right Of The Texture and Quad */
+    glTexCoord2f( 1.0f, 1.0f ); glVertex3f(  1.0f, -1.0f, 1.0f );
+    /* Top Right Of The Texture and Quad */
+    glTexCoord2f( 1.0f, 0.0f ); glVertex3f(  1.0f,  1.0f, 1.0f );
+    /* Top Left Of The Texture and Quad */
+    glTexCoord2f( 0.0f, 0.0f ); glVertex3f( -1.0f,  1.0f, 1.0f );
+    
+    /* Back Face */
+    /* Bottom Right Of The Texture and Quad */
+    glTexCoord2f( 0.0f, 0.0f ); glVertex3f( -1.0f, -1.0f, -1.0f );
+    /* Top Right Of The Texture and Quad */
+    glTexCoord2f( 0.0f, 1.0f ); glVertex3f( -1.0f,  1.0f, -1.0f );
+    /* Top Left Of The Texture and Quad */
+    glTexCoord2f( 1.0f, 1.0f ); glVertex3f(  1.0f,  1.0f, -1.0f );
+    /* Bottom Left Of The Texture and Quad */
+    glTexCoord2f( 1.0f, 0.0f ); glVertex3f(  1.0f, -1.0f, -1.0f );
+    
+    /* Top Face */
+    /* Top Left Of The Texture and Quad */
+    glTexCoord2f( 1.0f, 1.0f ); glVertex3f( -1.0f,  1.0f, -1.0f );
+    /* Bottom Left Of The Texture and Quad */
+    glTexCoord2f( 1.0f, 0.0f ); glVertex3f( -1.0f,  1.0f,  1.0f );
+    /* Bottom Right Of The Texture and Quad */
+    glTexCoord2f( 0.0f, 0.0f ); glVertex3f(  1.0f,  1.0f,  1.0f );
+    /* Top Right Of The Texture and Quad */
+    glTexCoord2f( 0.0f, 1.0f ); glVertex3f(  1.0f,  1.0f, -1.0f );
+    
+    /* Bottom Face */
+    /* Top Right Of The Texture and Quad */
+    glTexCoord2f( 0.0f, 1.0f ); glVertex3f( -1.0f, -1.0f, -1.0f );
+    /* Top Left Of The Texture and Quad */
+    glTexCoord2f( 1.0f, 1.0f ); glVertex3f(  1.0f, -1.0f, -1.0f );
+    /* Bottom Left Of The Texture and Quad */
+    glTexCoord2f( 1.0f, 0.0f ); glVertex3f(  1.0f, -1.0f,  1.0f );
+    /* Bottom Right Of The Texture and Quad */
+    glTexCoord2f( 0.0f, 0.0f ); glVertex3f( -1.0f, -1.0f,  1.0f );
+    
+    /* Right face */
+    /* Bottom Right Of The Texture and Quad */
+    glTexCoord2f( 0.0f, 0.0f ); glVertex3f( 1.0f, -1.0f, -1.0f );
+    /* Top Right Of The Texture and Quad */
+    glTexCoord2f( 0.0f, 1.0f ); glVertex3f( 1.0f,  1.0f, -1.0f );
+    /* Top Left Of The Texture and Quad */
+    glTexCoord2f( 1.0f, 1.0f ); glVertex3f( 1.0f,  1.0f,  1.0f );
+    /* Bottom Left Of The Texture and Quad */
+    glTexCoord2f( 1.0f, 0.0f ); glVertex3f( 1.0f, -1.0f,  1.0f );
+    
+    /* Left Face */
+    /* Bottom Left Of The Texture and Quad */
+    glTexCoord2f( 1.0f, 0.0f ); glVertex3f( -1.0f, -1.0f, -1.0f );
+    /* Bottom Right Of The Texture and Quad */
+    glTexCoord2f( 0.0f, 0.0f ); glVertex3f( -1.0f, -1.0f,  1.0f );
+    /* Top Right Of The Texture and Quad */
+    glTexCoord2f( 0.0f, 1.0f ); glVertex3f( -1.0f,  1.0f,  1.0f );
+    /* Top Left Of The Texture and Quad */
+    glTexCoord2f( 1.0f, 1.0f ); glVertex3f( -1.0f,  1.0f, -1.0f );
+  }
+  glEnd();
   
-  /* Back Face */
-  /* Bottom Right Of The Texture and Quad */
-  glTexCoord2f( 0.0f, 0.0f ); glVertex3f( -1.0f, -1.0f, -1.0f );
-  /* Top Right Of The Texture and Quad */
-  glTexCoord2f( 0.0f, 1.0f ); glVertex3f( -1.0f,  1.0f, -1.0f );
-  /* Top Left Of The Texture and Quad */
-  glTexCoord2f( 1.0f, 1.0f ); glVertex3f(  1.0f,  1.0f, -1.0f );
-  /* Bottom Left Of The Texture and Quad */
-  glTexCoord2f( 1.0f, 0.0f ); glVertex3f(  1.0f, -1.0f, -1.0f );
-  
-  /* Top Face */
-  /* Top Left Of The Texture and Quad */
-  glTexCoord2f( 1.0f, 1.0f ); glVertex3f( -1.0f,  1.0f, -1.0f );
-  /* Bottom Left Of The Texture and Quad */
-  glTexCoord2f( 1.0f, 0.0f ); glVertex3f( -1.0f,  1.0f,  1.0f );
-  /* Bottom Right Of The Texture and Quad */
-  glTexCoord2f( 0.0f, 0.0f ); glVertex3f(  1.0f,  1.0f,  1.0f );
-  /* Top Right Of The Texture and Quad */
-  glTexCoord2f( 0.0f, 1.0f ); glVertex3f(  1.0f,  1.0f, -1.0f );
-  
-  /* Bottom Face */
-  /* Top Right Of The Texture and Quad */
-  glTexCoord2f( 0.0f, 1.0f ); glVertex3f( -1.0f, -1.0f, -1.0f );
-  /* Top Left Of The Texture and Quad */
-  glTexCoord2f( 1.0f, 1.0f ); glVertex3f(  1.0f, -1.0f, -1.0f );
-  /* Bottom Left Of The Texture and Quad */
-  glTexCoord2f( 1.0f, 0.0f ); glVertex3f(  1.0f, -1.0f,  1.0f );
-  /* Bottom Right Of The Texture and Quad */
-  glTexCoord2f( 0.0f, 0.0f ); glVertex3f( -1.0f, -1.0f,  1.0f );
-  
-  /* Right face */
-  /* Bottom Right Of The Texture and Quad */
-  glTexCoord2f( 0.0f, 0.0f ); glVertex3f( 1.0f, -1.0f, -1.0f );
-  /* Top Right Of The Texture and Quad */
-  glTexCoord2f( 0.0f, 1.0f ); glVertex3f( 1.0f,  1.0f, -1.0f );
-  /* Top Left Of The Texture and Quad */
-  glTexCoord2f( 1.0f, 1.0f ); glVertex3f( 1.0f,  1.0f,  1.0f );
-  /* Bottom Left Of The Texture and Quad */
-  glTexCoord2f( 1.0f, 0.0f ); glVertex3f( 1.0f, -1.0f,  1.0f );
-  
-  /* Left Face */
-  /* Bottom Left Of The Texture and Quad */
-  glTexCoord2f( 1.0f, 0.0f ); glVertex3f( -1.0f, -1.0f, -1.0f );
-  /* Bottom Right Of The Texture and Quad */
-  glTexCoord2f( 0.0f, 0.0f ); glVertex3f( -1.0f, -1.0f,  1.0f );
-  /* Top Right Of The Texture and Quad */
-  glTexCoord2f( 0.0f, 1.0f ); glVertex3f( -1.0f,  1.0f,  1.0f );
-  /* Top Left Of The Texture and Quad */
-  glTexCoord2f( 1.0f, 1.0f ); glVertex3f( -1.0f,  1.0f, -1.0f );
-  glEnd( );
-  
-  /* Draw it to the screen */
-  SDL_GL_SwapBuffers( );
+  SDL_GL_SwapBuffers();
   
   /* Gather our frames per second */
-  Frames++;
+  ++frameCount;
   {
 	GLint t = SDL_GetTicks();
-	if (t - T0 >= 5000) {
-      GLfloat seconds = (t - T0) / 1000.0;
-      GLfloat fps = Frames / seconds;
-      printf("%d frames in %g seconds = %g FPS\n", Frames, seconds, fps);
-      T0 = t;
-      Frames = 0;
+	if (t - t0 >= 5000) {
+      GLfloat seconds = (t - t0) / 1000.0;
+      GLfloat fps = frameCount / seconds;
+      printf("%d frames in %g seconds = %g FPS\n", frameCount, seconds, fps);
+      t0 = t;
+      frameCount = 0;
 	}
   }
   
   xrot += 0.3f; /* X Axis Rotation */
   yrot += 0.2f; /* Y Axis Rotation */
   zrot += 0.4f; /* Z Axis Rotation */
-  
-  return true;
 }
 
 int main(int argc, char *argv[]) {
@@ -275,7 +244,7 @@ int main(int argc, char *argv[]) {
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
     fprintf( stderr, "Video initialization failed: %s\n",
             SDL_GetError( ) );
-    Quit(1);
+    cleanExit(1);
   }
   
   /* Fetch the video info */
@@ -284,7 +253,7 @@ int main(int argc, char *argv[]) {
   if (!videoInfo) {
     fprintf( stderr, "Video query failed: %s\n",
             SDL_GetError( ) );
-    Quit(1);
+    cleanExit(1);
   }
   
   /* the flags to pass to SDL_SetVideoMode */
@@ -319,7 +288,7 @@ int main(int argc, char *argv[]) {
   /* Verify there is a surface */
   if (!surface) {
     fprintf( stderr,  "Video mode set failed: %s\n", SDL_GetError( ) );
-    Quit(1);
+    cleanExit(1);
   }
   
   /* initialize OpenGL */
@@ -341,7 +310,7 @@ int main(int argc, char *argv[]) {
                                      SCREEN_BPP, videoFlags);
           if (!surface) {
             fprintf( stderr, "Could not get a surface after resize: %s\n", SDL_GetError( ) );
-            Quit(1);
+            cleanExit(1);
           }
           resizeWindow(event.resize.w, event.resize.h);
           break;
@@ -356,14 +325,12 @@ int main(int argc, char *argv[]) {
       }
     }
     
-    /* draw the scene */
-    drawGLScene();
+    drawScene();
   }
 finished:
   
-  /* clean ourselves up and exit */
-  Quit(0);
+  cleanExit(0);
   
-  /* Should never get here */
+  // Never reached.
   return 0;
 }
