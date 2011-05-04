ad9a11b7f2a29b641fd74bca5d033d807231429b
Steven Shaw <steven@steshaw.org>
2011-05-04 17:36:57 +1000

    Rename project

--- opengl/nehe/lesson06/lesson06.cpp
+++ opengl/nehe/lesson06/lesson06.cpp
@@ -1,4 +1,6 @@
 /*
+ * Adapted from original code. See below.
+ *
  * This code was created by Jeff Molofee '99
  * (ported to Linux/SDL by Ti Leggett '01)
  *
@@ -35,28 +37,79 @@ void cleanExit(int returnCode) {
   exit(returnCode);
 }
 
+static inline bool isPowerOf2(int i) {
+  return (i & (i - 1)) == 0;
+}
+
+static const char* textureFormatToString(GLenum textureFormat) {
+  switch (textureFormat) {
+    case GL_RGBA:
+      return "GL_RGBA";
+    case GL_BGRA:
+      return "GL_BGRA";
+      break;
+    case GL_RGB:
+      return "GL_RGB";
+    case GL_BGR:
+      return "GL_BGR";
+      break;
+    default:
+      return "unknown texture format";
+  }
+}
+    
 int loadTextures() {
-  SDL_Surface *textureImage[1];
-  textureImage[0] = SDL_LoadBMP("data/nehe.bmp");
-  if (!textureImage[0]) return false;
+  SDL_Surface *bitmap = SDL_LoadBMP("data/nehe.bmp");
+  if (!bitmap) return false;
+
+  if (!isPowerOf2(bitmap->w)) {
+    fprintf(stderr, "image width is not power of 2");
+    return false;
+  }
+  if (!isPowerOf2(bitmap->h)) {
+    fprintf(stderr, "image height is not power of 2");
+    return false;
+  }
   
+  int numColours = bitmap->format->BytesPerPixel;
+  GLenum texture_format;
+  if (numColours == 4) {
+    // Contains alpha channel.
+    if (bitmap->format->Rmask == 0x000000ff) {
+      texture_format = GL_RGBA;
+    } else {
+      texture_format = GL_BGRA;
+    }
+  } else if (numColours == 3) {
+    // No alpha channel.
+    if (bitmap->format->Rmask == 0x000000ff) {
+      texture_format = GL_RGB;
+    } else {
+      texture_format = GL_BGR;
+    }
+  } else {
+    fprintf(stderr, "Image is not true colour");
+    return false;
+  }
+  printf("texture format is '%s'\n", textureFormatToString(texture_format));
+          
   // Create the texture.
-  glGenTextures(1, &texture[0]);
+  glGenTextures(1, &texture[1]);
   
   // Typical texture generation using data from the bitmap.
   glBindTexture(GL_TEXTURE_2D, texture[0]);
-  
-  // Generate the texture.
-  glTexImage2D(GL_TEXTURE_2D, 0, 3, 
-               textureImage[0]->w, textureImage[0]->h, 
-               0, GL_BGR,
-               GL_UNSIGNED_BYTE, textureImage[0]->pixels);
-  
+
   // Linear Filtering.
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-  SDL_FreeSurface(textureImage[0]);
+  
+  // Generate the texture.
+  glTexImage2D(GL_TEXTURE_2D, 0, 
+               numColours, bitmap->w, bitmap->h, 
+               0, texture_format,
+               GL_UNSIGNED_BYTE, bitmap->pixels);
+  
+  SDL_FreeSurface(bitmap);
   return true;
 }
 
@@ -85,33 +138,6 @@ int resizeWindow(int width, int height) {
   return true;
 }
 
-void handleKeyPress(SDL_keysym *keysym) {
-  switch (keysym->sym) {
-	case SDLK_ESCAPE:
-    case SDLK_q:
-      printf("q/Esc pressed\n");
-      cleanExit(0);
-      break;
-    case SDLK_f:
-      printf("f pressed\n");
-      videoFlags |= SDL_FULLSCREEN;
-      surface = SDL_SetVideoMode(SCREEN_WIDTH,
-                                 SCREEN_HEIGHT,
-                                 SCREEN_BPP, videoFlags);
-      resizeWindow(SCREEN_WIDTH, SCREEN_HEIGHT);
-      break;
-	case SDLK_F1:
-      printf("F1 pressed\n");
-      SDL_WM_ToggleFullScreen(surface); // XXX: Seems to be broken...
-      break;
-	default:
-      break;
-  }
-  
-  return;
-}
-
-/* general OpenGL initialization function */
 int initGL() {
   if (!loadTextures()) return false;
   
@@ -131,7 +157,6 @@ int initGL() {
   return true;
 }
 
-/* Here goes our drawing code */
 void drawScene() {
   /* These are to calculate our fps */
   static GLint t0         = 0;
@@ -219,7 +244,8 @@ void drawScene() {
   }
   glEnd();
   
-  SDL_GL_SwapBuffers();
+  SDL_Flip(surface);
+//  SDL_GL_SwapBuffers();
   
   /* Gather our frames per second */
   ++frameCount;
@@ -239,68 +265,63 @@ void drawScene() {
   zrot += 0.4f; /* Z Axis Rotation */
 }
 
+void handleKeyPress(SDL_keysym *keysym) {
+  switch (keysym->sym) {
+	case SDLK_ESCAPE:
+    case SDLK_q:
+      printf("q/Esc pressed\n");
+      cleanExit(0);
+      break;
+    case SDLK_F1:
+    case SDLK_f:
+      printf("F1/f pressed - toggle fullscreen\n");
+      videoFlags ^= SDL_FULLSCREEN; // Toggle fullscreen bit.
+      surface = SDL_SetVideoMode(SCREEN_WIDTH,
+                                 SCREEN_HEIGHT,
+                                 SCREEN_BPP, videoFlags);
+      initGL();  
+      resizeWindow(SCREEN_WIDTH, SCREEN_HEIGHT);
+      break;
+	default:
+      break;
+  }
+  
+  return;
+}
+
 int main(int argc, char *argv[]) {
-  /* initialize SDL */
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-    fprintf( stderr, "Video initialization failed: %s\n",
-            SDL_GetError( ) );
+    fprintf(stderr, "Video initialization failed: %s\n", SDL_GetError());
     cleanExit(1);
   }
   
-  /* Fetch the video info */
-  const SDL_VideoInfo *videoInfo = SDL_GetVideoInfo( );
+  const SDL_VideoInfo *videoInfo = SDL_GetVideoInfo();
   
   if (!videoInfo) {
-    fprintf( stderr, "Video query failed: %s\n",
-            SDL_GetError( ) );
+    fprintf(stderr, "Video query failed: %s\n", SDL_GetError());
     cleanExit(1);
   }
+
+  videoFlags     |= SDL_DOUBLEBUF | SDL_HWSURFACE;
+  videoFlags     |= SDL_HWPALETTE;
+  videoFlags     |= SDL_RESIZABLE;
   
-  /* the flags to pass to SDL_SetVideoMode */
-  videoFlags     |= SDL_GL_DOUBLEBUFFER; /* Enable double buffering */
-  videoFlags     |= SDL_HWPALETTE;       /* Store the palette in hardware */
-  videoFlags     |= SDL_RESIZABLE;       /* Enable window resizing */
-//  videoInfo    |= SDL_FULLSCREEN;
-  
-  /* This checks to see if surfaces can be stored in memory */
-  if ( videoInfo->hw_available ) {
-    printf("Hardware surface\n");
-	videoFlags |= SDL_HWSURFACE;
-  } else {
-    printf("Software surface\n");
-	videoFlags |= SDL_SWSURFACE;
-  }
-  
-  /* This checks if hardware blits can be done */
-  printf("Hardware blit = %d\n", videoInfo->blit_hw);
-  if ( videoInfo->blit_hw ) {
-	videoFlags |= SDL_HWACCEL;
-  }
-  
-  /* Sets up OpenGL double buffering */
   SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
   
-  /* get a SDL surface */
   surface = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, videoFlags);
-  
-  printf("screen is hardware surface = %d\n", (surface->flags & SDL_HWSURFACE == 0));
-  
-  /* Verify there is a surface */
   if (!surface) {
-    fprintf( stderr,  "Video mode set failed: %s\n", SDL_GetError( ) );
+    fprintf(stderr, "Video mode set failed: %s\n", SDL_GetError());
     cleanExit(1);
   }
+
+  printf("screen is hardware surface = %d\n", (surface->flags & SDL_HWSURFACE == 0));
   
-  /* initialize OpenGL */
-  initGL();
-  
-  /* resize the initial window */
+  initGL();  
   resizeWindow(SCREEN_WIDTH, SCREEN_HEIGHT);
   
-  /* wait for events */
+  // Event loop.
   for (;;) {
-    /* handle the events in the queue */
-    SDL_Event event;    
+    SDL_Event event;
     while (SDL_PollEvent(&event)) {
       switch (event.type) {
         case SDL_VIDEORESIZE:
@@ -309,10 +330,11 @@ int main(int argc, char *argv[]) {
                                      event.resize.h,
                                      SCREEN_BPP, videoFlags);
           if (!surface) {
-            fprintf( stderr, "Could not get a surface after resize: %s\n", SDL_GetError( ) );
+            fprintf(stderr, "Could not get a surface after resize: %s\n", SDL_GetError());
             cleanExit(1);
           }
-          resizeWindow(event.resize.w, event.resize.h);
+          initGL();  
+          resizeWindow(SCREEN_WIDTH, SCREEN_HEIGHT);
           break;
         case SDL_KEYDOWN:
           handleKeyPress(&event.key.keysym);
