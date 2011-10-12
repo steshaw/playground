8de741ee096e2363982dac72ad5487430586b5c0
Steven Shaw <steven@steshaw.org>
2011-10-12 11:47:42 +1000

    Fixed bug but now the pattern for bottom of heirarchy is different. Must check if args need switching in subclasses

--- java/equals/etymon/Equals1Alternative.java
+++ java/equals/etymon/Equals1Alternative.java
@@ -5,32 +5,42 @@
 class Equals1Alternative {
 
   static class A {
-    int n1;
+    A(int a) { this.a = a; }
+    int a;
     public boolean equals(Object o) {
       if (super.equals(o)) {
+        // At the bottom of the heirarchy, return true if Object.equals is true. i.e. ==
         return true;
       } else {
         if (!(o instanceof A)) {
           return false;
         } else {
           A that = (A)o;
-          return this.n1 == that.n1;
+          return this.a == that.a;
         }
       }
     }
   }
 
-  static class B extends A { 
-    int n2;
+  static boolean isStrictSuperclass(Class<?> a, Class<?> b) {
+    return (b.isAssignableFrom(a) && b != a);
+  }
+
+  static class B extends A {
+    B(int a, int b) { super(a); this.b = b; }
+    int b;
     public boolean equals(Object o) {
-      if (super.equals(o)) {
-        return true;
+      // If o.class is a strict superclass of B, then "switch arguments" to equals.
+      if (o != null && isStrictSuperclass(B.class, o.getClass())) return o.equals(this);
+
+      if (!super.equals(o)) {
+        return false;
       } else {
         if (!(o instanceof B)) {
           return false;
         } else {
           B that = (B)o;
-          return this.n2 == that.n2;
+          return this.b == that.b;
         }
       }
     }
@@ -46,13 +56,50 @@ class Equals1Alternative {
 
   // And test it:
   public static void main(String[] args) {
-    A test1 = new A();
-    A test2 = new B();
-    printEqual(test1, test1); // => "eq"
-    printEqual(test2, test2); // => "eq"
-    printEqual(test1, test2); // => "eq"
-    printEqual(test2, test1); // => "eq"
-    printEqual(test1, null);  // => "ne"
-    printEqual(test2, null);  // => "ne"
+    A a1a = new A(1);
+    A a1b = new A(1);
+    A a2a = new A(2);
+    A a2b = new A(2);
+    A b12a = new B(1, 2);
+    A b12b = new B(1, 2);
+    A b13 = new B(1, 3);
+    A b23a = new B(2, 3);
+    A b23b = new B(2, 3);
+
+    // eq
+    printEqual(a1a, a1a);
+    printEqual(b12a, b12a);
+    printEqual(a1a, b12a);
+    printEqual(b12a, a1a);
+
+    printEqual(a1a, a1a);
+    printEqual(b12a, b12a);
+    printEqual(a1a, b12a);
+    printEqual(b12a, a1a);
+
+    printEqual(a1a, a1b);
+    printEqual(a1b, a1a);
+    printEqual(a2a, a2b);
+    printEqual(a2b, a2a);
+
+    printEqual(b12a, b12b);
+    printEqual(b12b, b12a);
+    printEqual(b23a, b23b);
+    printEqual(b23b, b23a);
+
+    // ne
+    printEqual(a1a, null);
+    printEqual(b12a, null);
+
+    printEqual(a1a, a2a);
+    printEqual(a2a, a1a);
+
+    printEqual(b12a, b13);
+    printEqual(b13,  b12a);
+
+    printEqual(a1a, b23a);
+    printEqual(b23a, a1a);
+    printEqual(a2a, b12a);
+    printEqual(b12a, a2a);
   }
 }
