b9683a446bbb7ec9427964ca12e5590701fa9d96
Steven Shaw <steven@steshaw.org>
2011-10-30 15:38:37 +1000

    Baby steps to a runnable example

--- java/maybe/CompilableSolution.java
+++ java/maybe/CompilableSolution.java
@@ -12,14 +12,14 @@ interface Monad<A> {
 
 final class Maybe<A> implements Monad<A> {
   public <B> Maybe<B> bind(final Monad<A> ma, final F<A, Monad<B>> f) {
-    if(ma instanceof Maybe) {
+    if (ma instanceof Maybe) {
       final A j = ((Maybe<A>)ma).just();
       if (j == null) {
         return new Maybe<B>();
       } else {
         final Monad<B> b = f.f(j);
 
-        if(b instanceof Maybe) {
+        if (b instanceof Maybe) {
           return (Maybe<B>)b;
         } else {
           throw new Error("Just because we don't have higher-order types," +
@@ -51,5 +51,50 @@ final class Maybe<A> implements Monad<A> {
 
   public A just() {
     return a;
+  }
+
+  @Override public String toString() {
+    if (a == null) return "Nothing"; else return "Just " + a.toString();
+  }
+}
+
+class Person {
+  final String name;
+  final int age;
+
+  Person(String name, int age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  @Override public String toString() {
+    return "Person(name = '" + name + "' age = " + age + ")";
+  }
+
+  static Person mk(String name, int age) {
+    return new Person(name, age);
+  }
+}
+
+class Main {
+  static Maybe<String> couldBeName() {
+    return new Maybe<String>("Steve");
+  }
+
+  static Maybe<Integer> couldBeAge() {
+    return new Maybe<Integer>(40);
+  }
+
+  static Maybe<Person> couldBePerson(Maybe<String> maybeName, Maybe<Integer> maybeAge) {
+    return maybeName.bind(/* XXX - why is this param required? */ (Monad<String>)maybeName, new F<String, Monad<Person>>() {
+      @Override public Monad<Person> f(String name) {
+        System.out.println("a1 - name = " + name);
+        return (Monad<Person>) new Maybe<Person>(Person.mk(name, 40));
+      }
+    });
+  }
+
+  public static void main(String[] args) {
+    System.out.println(couldBePerson(couldBeName(), couldBeAge()));
   }
 }
