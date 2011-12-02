89ff55965fc2a60d4eb98e0b65cfcc27ae8e6f84
Steven Shaw <steven@steshaw.org>
2011-12-02 14:54:58 +1000

    Proper object initialisation from deep-c

---
+++ c/deep-c/proper_object_initialisation.cc
@@ -0,0 +1,20 @@
+#include <cstdio>
+
+struct A {
+  A() { puts("A()"); }
+  A(int v) { puts("A(int)"); }
+ ~A() { puts("~A()"); }
+};
+
+struct X {
+  X(int  v) { a = A(v); }
+  X(long v) : a(v) {}
+  A a;
+};
+
+int main() {
+  puts("bad style");
+  { X slow(int(2)); }
+  puts("good style");
+  { X fast(long(2)); }
+}
