aafcf0ac6288d61a6477a71329c36caa14bd085e
Steven Shaw <steven@steshaw.org>
2011-12-31 13:30:54 +1000

    Add pointer size

--- c/sizes/sizes.c
+++ c/sizes/sizes.c
@@ -4,5 +4,6 @@ int main() {
   printf("sizeof(short) = %ld\n", sizeof(short));
   printf("sizeof(int) = %ld\n", sizeof(int));
   printf("sizeof(long) = %ld\n", sizeof(long));
+  printf("sizeof(void*) = %ld\n", sizeof(void*));
   return 0;
 }
