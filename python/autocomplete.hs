8c1a55f1c5146514a5d79d32b37c9a829cc1e614
Steven Shaw <steven@steshaw.org>
2012-03-27 14:35:00 +1000

    Haskell port of autocomplete

---
+++ python/autocomplete.hs
@@ -0,0 +1,57 @@
+--
+-- Port to Haskell of original Python code from
+--
+--   http://williamedwardscoder.tumblr.com/post/18319031919/programming-language-readability
+--
+
+{-
+ - A fast data structure for searching strings with autocomplete support.
+ -}
+module Trie where
+
+import qualified Data.Map as M
+
+data Trie = 
+  Trie { value :: String
+       , children :: M.Map Char Trie
+       , terminates :: Bool
+       }
+  deriving (Show, Eq)
+
+empty :: Trie
+empty = Trie {value = "", children = M.empty, terminates = False}
+
+insert :: String -> Trie -> Trie
+insert [] t = t { value = value t, terminates = True }
+insert (k:ks) t =
+  let cs = children t
+  in case M.lookup k cs of
+        Nothing -> t { children = M.insert k (insert ks child) cs }
+                   where child = empty {value = value t ++ [k]}
+        Just t' -> t { children = M.insert k (insert ks t') cs }
+
+findNode :: String -> Trie -> Maybe Trie
+findNode [] t = Just $ t
+findNode (k:ks) t =
+  case M.lookup k (children t) of
+    Nothing -> Nothing
+    Just t' -> findNode ks t'
+
+find :: String -> Trie -> Maybe String
+find w t = case findNode w t of
+  Nothing -> Nothing
+  Just t' -> if terminates t' then Just $ value t' else Nothing
+
+allPrefixes :: Trie -> [String]
+allPrefixes t =
+  if terminates t
+  then value t : cs else cs
+  where
+    cs = M.fold f [] (children t)
+    f n as = as ++ (allPrefixes n)
+
+autoComplete :: String -> Trie -> [String]
+autoComplete prefix t = 
+  case findNode prefix t of
+    Nothing -> []
+    Just n -> allPrefixes n
