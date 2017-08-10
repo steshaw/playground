fa665386d17ebc372a26d9225d096a257fc725b8
Steven Shaw <steven@steshaw.org>
2017-08-10 16:33:59 +1000

    gofmt and continuation lines

---
+++ go/continuation-line.go
@@ -0,0 +1,12 @@
+//
+// Just playing around with long/continuation lines and gofmt.
+//
+package foo
+
+func main() {
+	err := database.QueryRow(
+		"select * from users where user_id=?", id,
+	).Scan(
+		&ReadUser.ID, &ReadUser.Name, &ReadUser.First, &ReadUser.Last, &ReadUser.Email,
+	)
+}
