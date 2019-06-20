306c1de302703fab2215166eeada37595c09eb15
Steven Shaw <steven@steshaw.org>
2019-06-20 19:59:08 +1000

    🎨 Format with tsfmt

--- typescript/greeter.ts
+++ typescript/greeter.ts
@@ -1,17 +1,17 @@
 class Student {
-    fullName: string;
-    constructor(public firstName, public middleInitial, public lastName) {
-        this.fullName = firstName + " " + middleInitial + " " + lastName;
-    }
+  fullName: string;
+  constructor(public firstName, public middleInitial, public lastName) {
+    this.fullName = firstName + " " + middleInitial + " " + lastName;
+  }
 }
 
 interface Person {
-    firstName: string;
-    lastName: string;
+  firstName: string;
+  lastName: string;
 }
 
-function greeter(person : Person) {
-    return "Hello, " + person.firstName + " " + person.lastName;
+function greeter(person: Person) {
+  return "Hello, " + person.firstName + " " + person.lastName;
 }
 
 var user = new Student("Jane", "M.", "User");
