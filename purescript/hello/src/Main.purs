f7aa6645e90b67acc44f5c65795bb038c141b284
Steven Shaw <steven@steshaw.org>
2019-06-30 17:03:52 +1000

    ✨ Use code from hello.purs

--- purescript/hello/src/Main.purs
+++ purescript/hello/src/Main.purs
@@ -1,3 +1,4 @@
+{-
 module Main where
 
 import Prelude
@@ -7,3 +8,22 @@ import Effect.Console (log)
 main :: Effect Unit
 main = do
   log "Hello sailor!"
+-}
+
+module Main where
+
+import Prelude
+
+import Effect (Effect)
+import Effect.Console (log)
+
+times :: Int -> Effect Unit -> Effect Unit
+times n action =
+  if (n == 0)
+  then log "done"
+  else do
+    action
+    times (n-1) action
+
+main :: Effect Unit
+main = times 10 $ log "Hello, PureScript!"
