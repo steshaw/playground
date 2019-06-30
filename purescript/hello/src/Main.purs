89de7811ad8f49e1e53500d33013d8299d7d86a6
Steven Shaw <steven@steshaw.org>
2019-06-30 18:17:19 +1000

    Trying to blow stack in PureScript

--- purescript/hello/src/Main.purs
+++ purescript/hello/src/Main.purs
@@ -8,14 +8,26 @@ import Effect.Console (log)
 
 times :: Int -> Effect Unit -> Effect Unit
 times n action = replicateM_ n action
-{-
-times n action =
+
+-- Potential stack overflow can happen with large values of n.
+timesSO :: Int -> Effect Unit -> Effect Unit
+timesSO n action =
   if (n == 0)
-  then log "done"
+  then log "done" -- FIXME: Not seeing "done".
   else do
     action
     times (n-1) action
--}
 
 main :: Effect Unit
-main = times 10 $ log "Hello, PureScript!"
+main = do
+  let n = 2147483647
+
+  log "times 10"
+  times 10 $ log "Hello, PureScript!"
+  log $ "timesSO 10"
+  timesSO 10 $ log "Hello, PureScript!"
+
+  log $ "times " <> show n
+  times n $ pure unit
+  log $ "timesSO " <> show n
+  timesSO n $ pure unit
